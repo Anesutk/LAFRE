4effaacdc8c2adafb91029239e1c0e7857322ede
Anesutk <anesutmandebvu@gmail.com>
2026-08-27 11:15:04 -0700

    static see

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
 
+python manage.py collectstatic --noinput --clear
 python -m gunicorn backend.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers ${WEB_CONCURRENCY:-2} --timeout ${GUNICORN_TIMEOUT:-180}
